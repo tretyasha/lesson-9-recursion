deef674fd77989633861ba8b81036c4af37d1eee
tretyasha <72045043+tretyasha@users.noreply.github.com>
2023-03-02 16:36:55 +0300

    задача 5 решена

---
+++ task5/Program.cs
@@ -0,0 +1,16 @@
+﻿// Написать программу возведения числа А в целую стень B
+Console.Write("Введите A: ");
+int A = int.Parse(Console.ReadLine());
+Console.Write("Введите B: ");
+int B = int.Parse(Console.ReadLine());
+
+int result = Degree(A, B);
+int Degree(int A, int B)
+{
+    if (B > 1)
+        return A * Degree(A, B - 1);
+    else
+        return A;
+}
+
+Console.WriteLine($" {result}");
