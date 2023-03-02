f8324fe83c8557c55d3d2d80a70b787b07974309
tretyasha <72045043+tretyasha@users.noreply.github.com>
2023-03-02 16:03:06 +0300

    задача 4 решена

---
+++ task4/Program.cs
@@ -0,0 +1,9 @@
+﻿// Написать программу вычисления функции Аккермана
+int Akk(int n, int m)
+{
+    if (n == 0) return m + 1;
+    if (n != 0 && m == 0) return Akk(n - 1, 1);
+    if (n > 0 && m > 0) return Akk(n - 1, Akk(n, m - 1));
+    return Akk(n, m);
+}
+Console.WriteLine(Akk(2, 1));
