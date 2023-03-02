67fa9bd6bd775f9b484fcce75bd8be8df5e2903a
tretyasha <72045043+tretyasha@users.noreply.github.com>
2023-03-02 16:41:12 +0300

    задача2 исправлена

--- task2/Program.cs
+++ task2/Program.cs
@@ -1,7 +1,7 @@
-﻿// Найти сумму элементов от M до N, N и M заданы
-int SumRec(int n, int m)
+﻿// Показать натуральные числа от M до N, N и M заданы
+string NumbersRec(int n, int m)
 {
-    if (n == 0) return 0;
-    else return SumRec(n - 1, m) + n;
+    if (n <= m) return NumbersRec(n + 1, m) + $"{n}";
+    else return String.Empty;
 }
-Console.WriteLine(SumRec(9, 1));
+Console.WriteLine(NumbersRec(1, 9));
