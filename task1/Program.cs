44421207b42b30a7489f5c026a3cfdeac2f80977
tretyasha <72045043+tretyasha@users.noreply.github.com>
2023-03-02 15:54:58 +0300

    первая задача решена

---
+++ task1/Program.cs
@@ -0,0 +1,8 @@
+﻿// Показать натуральные числа от M до N, N и M заданы
+int SumRec(int n, int m)
+{
+    if (n == 0) return 0;
+    else return SumRec(n - 1, m) + n;
+}
+Console.WriteLine(SumRec(9, 1));
+
