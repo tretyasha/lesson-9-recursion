54a6069f183edad717ac9312f60d71a5e7015fe1
tretyasha <72045043+tretyasha@users.noreply.github.com>
2023-03-02 16:40:17 +0300

    задача 3 решена

--- task3/Program.cs
+++ task3/Program.cs
@@ -1,1 +1,14 @@
 ﻿// Найти сумму цифр числа
+
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+int sum = Summ(number);
+int Summ(int number)
+{
+    if (number > 0)
+        return
+        number % 10 + Summ(number / 10);
+    else
+        return 0;
+}
+Console.WriteLine($" {sum}");
